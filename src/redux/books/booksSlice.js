d666a856fefb47f6d8d88d7c88d2d8393501c9a0
Feliperto <fperezferraro@gmail.com>
2023-08-21 20:06:42 -0300

    created the booksSlice file

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,26 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const initialState = {
+  books: [],
+};
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      const newBook = {
+        title: action.payload.title,
+        category: action.payload.category,
+        id: state.books.length,
+      };
+      state.books.push(newBook);
+    },
+    removeBook: (state, action) => {
+      state.books = state.books.filter((book) => book.id !== action.payload);
+    },
+  },
+});
+
+export const { addBook, removeBook } = booksSlice.actions;
+export default booksSlice.reducer;
