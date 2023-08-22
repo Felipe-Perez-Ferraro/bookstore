d8d220315ffe1dd3f575208f6b9b8b7f1bc56e2b
Feliperto <fperezferraro@gmail.com>
2023-08-22 11:25:28 -0300

    diplaying the author

--- src/components/bookList/BookCard.jsx
+++ src/components/bookList/BookCard.jsx
@@ -5,7 +5,16 @@ function BookCard({ book, deleteBook }) {
   return (
     <div className="bookCard">
       <h2 className="bookCard__title">{book.title}</h2>
-      <p className="bookCard__title">{book.category}</p>
+      <p className="bookCard__author">
+        <span className="bookCard__bold">Author:</span>
+        {' '}
+        {book.author}
+      </p>
+      <p className="bookCard__category">
+        <span className="bookCard__bold">Category:</span>
+        {' '}
+        {book.category}
+      </p>
       <button
         type="button"
         onClick={() => deleteBook(book.id)}
@@ -21,6 +30,7 @@ BookCard.propTypes = {
   deleteBook: PropTypes.func.isRequired,
   book: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
   }).isRequired,
