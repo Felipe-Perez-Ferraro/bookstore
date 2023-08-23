19000556ec7795040131fddebd568c0c5ab93658
Feliperto <fperezferraro@gmail.com>
2023-08-23 18:36:46 -0300

    added the getBooks dispatch

--- src/components/bookList/BookList.jsx
+++ src/components/bookList/BookList.jsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import BookCard from './BookCard';
 import './BookList.css';
+import { fetchBooks } from '../../redux/books/booksSlice';
 
-function BookList({ books, deleteBook }) {
+function BookList() {
+  const dispatch = useDispatch();
+  const books = useSelector((state) => Object.values(state.books).flat());
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
   if (books.length === 0) {
     return <h2 className="bookList__message">There isn&lsquo;t Book yet...</h2>;
   }
   return (
     <section className="bookCard__section">
       <article className="bookCard__section__container">
-        {books.map((book) => (
-          <BookCard key={book.id} book={book} deleteBook={deleteBook} />
+        {Array.isArray(books) && books.map((book) => (
+          <BookCard
+            key={book.item_id}
+            title={book.title}
+            author={book.author}
+            category={book.category}
+            id={book.item_id}
+          />
         ))}
       </article>
     </section>
   );
 }
-
-BookList.propTypes = {
-  deleteBook: PropTypes.func.isRequired,
-  books: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-};
 
 export default BookList;
