04b51987d965f94fcf941e9238e65173260051ab
Felipe <fperezferraro@gmail.com>
2023-08-23 15:17:38 -0300

    Added usersSlice to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksSlice from './books/booksSlice';
 import categoriesSlice from './categories/categoriesSlice';
+import usersSlice from './users/usersSlice';
 
 const store = configureStore({
   reducer: {
     books: booksSlice,
     categories: categoriesSlice,
+    users: usersSlice,
   },
 });
 
