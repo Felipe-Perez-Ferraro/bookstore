8c246da8e165c93fea4df8c57c8dd45232c0e23c
Feliperto <fperezferraro@gmail.com>
2023-08-23 18:37:07 -0300

    deleting the child props

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -1,25 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
 import BookForm from '../../components/bookForm/BookForm';
 import BookList from '../../components/bookList/BookList';
 import './Categories.css';
-import { addBook, removeBook } from '../../redux/books/booksSlice';
 
 function Categories() {
-  const books = useSelector((state) => state.books.books);
-  const dispatch = useDispatch();
-
-  const createBook = (book) => {
-    dispatch(addBook(book));
-  };
-
-  const deleteBook = (bookId) => {
-    dispatch(removeBook(bookId));
-  };
-
   return (
     <main className="main__section">
-      <BookForm createBook={createBook} />
-      <BookList books={books} deleteBook={deleteBook} />
+      <BookForm />
+      <BookList />
     </main>
   );
 }
